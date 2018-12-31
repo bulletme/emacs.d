d44a53549fe4b35fb77864fda4a3ef4f1281d43b
Todd Yamakawa <todd.yamakawa@arm.com>
2018-12-31 12:01:28 -0600

    my-org: Adding org-bullets.

--- config/my-org.el
+++ config/my-org.el
@@ -1,9 +1,23 @@
 ;; =============================================================================
 ;; ORG MODE
 ;; =============================================================================
-(setq org-todo-keywords '((sequence "TODO" "WIP" "HOLD" "|" "DONE")))
 
-; Use indentation for readability
+;; Use manoj-dark theme
+(add-hook 'org-mode-hook (lambda ()
+	(load-theme 'manoj-dark t)
+	; FIXME: Figure out why org-bullets has ugly background in terminal mode
+	(if window-system (progn
+		(use-package org-bullets
+			:ensure t
+			:config (org-bullets-mode 1)
+		)
+	))
+))
+
+;; Use indentation for readability
 (with-eval-after-load 'org (setq org-startup-indented t))
+
+;; Add todo keywords
+(setq org-todo-keywords '((sequence "TODO" "WIP" "HOLD" "|" "DONE")))
 
 (provide 'my-org)
